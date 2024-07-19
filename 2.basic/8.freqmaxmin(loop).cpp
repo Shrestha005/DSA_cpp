e2c488efefe0d0ad6ab92ea5e5673cda8fda3b27
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-07-19 20:29:53 +0530

    commit

---
+++ 2.basic/8.freqmaxmin(loop).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int arr[] = {10, 5, 10, 15, 10, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    countFreq(arr, n);
+    return 0;
+}
