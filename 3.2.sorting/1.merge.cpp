38fee95352121b3ae7dd5c851d8633874d112ddc
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-07-21 11:11:55 +0530

    commit

--- 3.2.sorting/1.merge.cpp
+++ 3.2.sorting/1.merge.cpp
@@ -1,22 +1,62 @@
 #include<bits/stdc++.h>
 using namespace std;         
 
-
-
-
-int main(){
-    int n;
-    cin>> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+void merge(vector<int> &arr, int lo, int mid, int hi){
+    vector<int> temp;
+    int left= lo;
+    int right= mid+1;
+    while (left<= mid && right<= hi)
     {
-        cin>> arr[i];
+        if (arr[left]<=arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }  
     }
-    sort(arr, n);
-
-    for (int i = 0; i < n; i++)
+    while (left<=mid)
     {
-        cout<< arr[i]<< endl;
+        temp.push_back(arr[left]);
+        left++;
     }
-    return 0;
+    while (right<=hi)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for (int i = lo; i <= hi; i++)
+    {
+        arr[i]= temp[i-lo];
+    } 
+}
+void mergeSort(vector<int> &arr, int lo, int hi){
+    if(lo== hi) return;
+    int mid= (lo+ hi)/2;
+    mergeSort(arr, lo, mid);
+    mergeSort(arr, mid+1, hi);
+    merge(arr, lo, mid, hi);
+}
+
+
+int main() {
+
+    vector<int> arr = {9, 4, 7, 6, 3, 1, 5}  ;
+    int n = 7;
+
+    cout << "Before Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " "  ;
+    }
+    cout << endl;
+    return 0 ;
 }
