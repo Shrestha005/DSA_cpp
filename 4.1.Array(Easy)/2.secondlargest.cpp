c9f048975b741461cda12e17e2ca4aedfdfdd39c
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-07-22 21:46:54 +0530

    commit

--- 4.1.Array(Easy)/2.secondlargest.cpp
+++ 4.1.Array(Easy)/2.secondlargest.cpp
@@ -1,13 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int second_largest(int arr[], int n){}
+int second_largest(int arr[], int n){
+    int largest= arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]> largest)
+        {
+            largest= arr[i];
+        } 
+    }
+    int s_largest= -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i]> s_largest && arr[i]!= largest)
+        {
+            s_largest= arr[i];
+        }   
+    } 
+    return s_largest;   
+}
 
 
 int main(){
-    int arr[]= {2,5,9,0,14};
+    int arr[]= {2,5,9,11,14};
+    // int arr[]= {2,2,2,2,2};
     int n= 5;
-    int max= second_largest(arr, n);
-    cout<< "The second largest element in the array is : "<< max<< endl;
+    int secondLargest = second_largest(arr, n);
+    cout<< "The second largest element in the array is : "<< secondLargest<< endl;
     return 0;
 }
