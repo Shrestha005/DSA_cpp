958315079295d9e553e072c6b96329ca8e9b5163
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-09-14 13:48:37 +0530

    commit

---
+++ 4.1.Array(Easy)/8.linear_search.cpp
@@ -0,0 +1,20 @@
+#include <iostream>  // Include iostream instead of stdio.h
+
+using namespace std; // Use the standard namespace
+
+int search(int arr[], int n, int num) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == num)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int num = 4;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int val = search(arr, n, num);
+    
+    cout << val; // Use cout instead of printf
+}
