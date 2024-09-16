1cbded3ede7aaecfc03019cf729d8dde99f5e03a
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-09-16 20:12:25 +0530

    commit

--- 4.1.Array(Easy)/9.2.union.cpp
+++ 4.1.Array(Easy)/9.2.union.cpp
@@ -44,9 +44,12 @@ vector <int> findUnion(int arr1[], int arr2[], int n1, int n2){
 
 int main()
 {
-  int n1 = 10, n2 = 7;
-  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
+//   int n1 = 10, n2 = 7;
+//   int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//   int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
+  int n1 = 6, n2 = 2;
+  int arr1[] = {1,1,1,2,3,3};
+  int arr2[] = {0,4}; 
   vector < int > Union = findUnion(arr1, arr2, n1, n2);
   cout << "Union of arr1 and arr2 is  " << endl;
   for (auto & val: Union)
