e03eea0465bf0e8b89647883a061f4f1603e0403
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-09-30 21:30:17 +0530

    commit

--- 4.1.Array(Easy)/13.2.num_appears_once.cpp
+++ 4.1.Array(Easy)/13.2.num_appears_once.cpp
@@ -1,1 +1,38 @@
 //better approach 1
+//Array hashing:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr){
+    int n= arr.size(); // size of array
+
+    int maxi= arr[0]; // find max element
+    for (int i = 0; i < n; i++)
+    {
+        maxi= max(arr[i], maxi);
+    }
+    //array of size maxi+1
+    //hash array
+    vector<int> hash(maxi+1, 0);
+        for (int i = 0; i < n; i++)
+        {
+            hash[arr[i]]++;
+        }
+        
+    //answer
+    for (int i = 0; i < n; i++) {
+        if (hash[arr[i]] == 1)
+            return arr[i];
+        }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
+
