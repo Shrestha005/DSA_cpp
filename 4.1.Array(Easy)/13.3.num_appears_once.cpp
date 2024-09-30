74d753059f5ab11bdf5ce683e185b6639ccc3084
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-09-30 23:36:39 +0530

    commit

--- 4.1.Array(Easy)/13.3.num_appears_once.cpp
+++ 4.1.Array(Easy)/13.3.num_appears_once.cpp
@@ -1,1 +1,23 @@
 //optimal (XOR)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int> &arr) {
+    int n = arr.size();
+
+    int xorr= 0;
+    for (int i = 0; i < n; i++)
+    {
+        xorr= xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    int ans = getSingleElement(arr);
+    cout << "The single element is: " << ans << endl;
+    return 0;
+}
