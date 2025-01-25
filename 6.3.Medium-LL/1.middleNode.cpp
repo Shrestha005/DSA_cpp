109220e894d0a4974b050e79f5ee55c66e8c62bc
Shrestha Mukherjee <shresthamukh07@gmail.com>
2025-01-26 00:08:23 +0530

    cpp

--- 6.3.Medium-LL/1.middleNode.cpp
+++ 6.3.Medium-LL/1.middleNode.cpp
@@ -19,25 +19,6 @@ class Node{
     }
 };
 
-Node* convertArrToDLL(vector<int> &arr){
-    Node* head= new Node(arr[0]);
-    Node* prev= head;
-    for(int i= 1; i< arr.size(); i++){
-        Node* temp= new Node(arr[i],nullptr,prev);
-        prev->next=temp;
-        prev=prev->next; //or prev=temp
-    }
-    return head;
-}
-
-void printLL(Node* head){
-    while (head!=NULL)
-    {
-        cout<< head->data<< " ";
-        head= head->next;
-    }
-}
-
 
 //Brute
 // Node *findMiddle(Node *head) {
@@ -59,3 +40,32 @@ void printLL(Node* head){
 //     }
 //     return temp;
 // }
+
+//Optimal(called Tortoise and Hare Algorithm)
+//slow(+1) and fast(+2) ptr
+//when fast is at the last node or has crossed the last node, the slow stands at mid. Return slow.
+Node *findMiddle(Node *head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    Node* slow= head;
+    Node* fast= head;
+    while (fast!=NULL && fast->next!=NULL)
+    {
+        slow= slow->next;
+        fast= fast->next->next;
+    }
+    return slow;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    Node* middleNode = findMiddle(head);
+    cout << "The middle node value is: " << middleNode->data << endl;
+    return 0;
+}
+     
